9137135c59f08d339decc42ef1949354c7f3eafe
Lena <90480521+LenaGavrilova@users.noreply.github.com>
2023-05-18 13:41:04 +0300

    Update Config.java

--- src/main/java/ru/kpfu/itis/config/Config.java
+++ src/main/java/ru/kpfu/itis/config/Config.java
@@ -65,8 +65,8 @@ public class Config {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
-        properties.put("hibernate.enable_lazy_load_no_trans", env.getRequiredProperty("hibernate.enable_lazy_load_no_trans"));
+//         properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+//         properties.put("hibernate.enable_lazy_load_no_trans", env.getRequiredProperty("hibernate.enable_lazy_load_no_trans"));
 
         return properties;
     }
